25c66f4b2e8c2ef0adab953116ec20452f5cef69
jojobear99 <jojobear99@685d4855-3b5d-4a0b-8599-13373f98f8b9>
2012-02-29 00:41:52 +0000

    Check in initial version of source files required for generating PopTrayU.chm (help file)

---
+++ trunk/help/source/header.js
@@ -0,0 +1,35 @@
+var startcontent = '<table width="100%" cellspacing="0" cellpadding="0" border="0" class="header">'+
+						'<tr>'+
+						'<td rowspan="2" align="center" width="120">'+
+						'<table cellspacing="4" cellpadding="0" border="0">'+
+						'<tr>'+
+						'<td class="headerinset"><a href="poptray.htm"><img src="img/logo.png" border="0" alt="PopTrayU home" width="55" height="55"></a></td>'+
+						'</tr>'+
+						'</table>'+
+						'</td>'+
+						'<td align="center" valing="bottom" class="head">PopTrayU</td>'+
+						'<td rowspan="2" align="center" width="120">'+
+						'<table cellspacing="4" cellpadding="0" border="0">'+
+						'<tr>'+
+						'<td><img src="img/cleardot.gif" border="0" width="55" height="55"></td>'+
+						'</tr>'+
+						'</table>'+
+						'</td>'+
+						'</tr>'+
+						'<tr>'+
+						'<td align="center" valign="top" class="subhead">FREEWARE Mail Notifier</td>'+
+						'</tr>'+
+						'</table>'+
+						'<table cellspacing="0" cellpadding="0" border="0"><tr><td class="small">&nbsp;</td></tr></table>'+
+						'<table cellspacing="0" cellpadding="0" border="0">'+
+						'<tr>'+
+						'<td class="small">&nbsp;</td>'+
+						'</tr>'+
+						'</table>'+
+						'<table width="100%" border="0" cellpadding="0" cellspacing="0">'+
+						'<tr>'+
+						'<td class="front">'+
+						'<table width="100%" border="0" cellpadding="0" cellspacing="0" height="100">'+
+						'<tr>'+
+						'<td class="inset">'
+document.write(startcontent)
