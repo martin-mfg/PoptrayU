e5a8906fb999fdd2b7f703e096dce4f8bd1f127f
jojobear99 <jojobear99@685d4855-3b5d-4a0b-8599-13373f98f8b9>
2015-10-17 20:59:35 +0000

    Refactor Export Account Dialog to handle exporting a single account as well.

--- source/ExportAcctDlg.pas
+++ source/ExportAcctDlg.pas
@@ -4,7 +4,7 @@ interface
 
 uses Winapi.Windows, System.SysUtils, System.Classes, Vcl.Graphics, Vcl.Forms,
   Vcl.Controls, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtCtrls, OKCANCL2, Vcl.CheckLst,
-  Vcl.ComCtrls;
+  Vcl.ComCtrls, uAccounts;
 
 type
   TExportAccountsDlg = class(TOKRightDlg)
@@ -17,17 +17,20 @@ type
     procedure chkSelAllClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure OKBtnClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
+    function DoExport(saveDialogTitle : String): integer;
   public
     { Public declarations }
+    function ExportSingleAccount(account : TAccount) : integer;
   end;
 
 var
   ExportAccountsDlg: TExportAccountsDlg;
 
 implementation
-uses uAccounts, System.IniFiles, Vcl.Dialogs, uTranslate;
+uses System.IniFiles, Vcl.Dialogs, uTranslate;
 
 {$R *.dfm}
 
@@ -44,12 +47,19 @@ begin
 end;
 
 procedure TExportAccountsDlg.FormCreate(Sender: TObject);
+begin
+  inherited;
+  ListViewAccounts.Clear;
+end;
+
+procedure TExportAccountsDlg.FormShow(Sender: TObject);
 var
   account : TAccount;
   item : TListItem;
 begin
   inherited;
-  ListViewAccounts.Clear;
+  TranslateComponentFromEnglish(self);
+
   for account in Accounts do begin
     item := ListViewAccounts.Items.Add;
     item.Caption := account.Name;
@@ -57,8 +67,22 @@ begin
     item.SubItems.Add(account.Login);
     item.Data := account;
   end;
+end;
 
-  TranslateComponentFromEnglish(self);
+// return value = mrOk or mrCancel (whether user pressed cancel on the save dialog)
+function TExportAccountsDlg.ExportSingleAccount(account : TAccount) : integer;
+var
+  item : TListItem;
+begin
+  ListViewAccounts.Clear;
+  item := ListViewAccounts.Items.Add;
+  item.Caption := account.Name;
+  //item.SubItems.Add(account.Server);
+  //item.SubItems.Add(account.Login);
+  item.Data := account;
+  item.Selected := true;
+
+  Result := DoExport(Translate('Backup Account:')+' '+account.name);
 end;
 
 
@@ -67,7 +91,8 @@ begin
   Application.HelpContext(HelpContext);
 end;
 
-procedure TExportAccountsDlg.OKBtnClick(Sender: TObject);
+// return value = mrOk or mrCancel (whether user pressed cancel on the save dialog)
+function TExportAccountsDlg.DoExport(saveDialogTitle : String): integer;
 var
   saveDialog : TSaveDialog;    // Save dialog variable
   iniSection : string;
@@ -75,12 +100,10 @@ var
   numExportedAccounts : integer;
   item : TListItem;
 begin
-  inherited;
-
   saveDialog := TSaveDialog.Create(self);
   try
-    saveDialog.Title := Translate('Export/Backup Accounts');
     saveDialog.InitialDir := GetCurrentDir;
+    saveDialog.Title := saveDialogTitle;
     saveDialog.Filter := Translate('Ini File') + '|*.ini|' + Translate('Text File') + '|*.txt';
     saveDialog.DefaultExt := 'ini';
     saveDialog.FilterIndex := 1;
@@ -88,8 +111,7 @@ begin
     // Display the save file dialog
     if saveDialog.Execute
     then begin
-      //ShowMessage('File : '+saveDialog.FileName);
-
+      Result := mrOk;
       // write to ini
       Ini := TMemIniFile.Create(saveDialog.FileName);
       numExportedAccounts := 0;
@@ -103,21 +125,23 @@ begin
             Ini.UpdateFile;
           end;
         end;
-
         Ini.WriteInteger('Options','NumAccounts',numExportedAccounts);
       finally
          Ini.Free;
-
       end;
-
       ShowMessage('Account(s) Exported Successfully'+ sLineBreak + saveDialog.FileName);
 
-    end;
+    end else Result := mrCancel;
   finally
     saveDialog.Free;
   end;
+end;
 
+procedure TExportAccountsDlg.OKBtnClick(Sender: TObject);
+begin
+  inherited;
+  DoExport(Translate('Export/Backup Accounts'));
 end;
 
 end.
- 
+
