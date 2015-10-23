702270f33fb1dde041437823e1a9a5153f256d53
jojobear99 <jojobear99@685d4855-3b5d-4a0b-8599-13373f98f8b9>
2015-10-23 16:37:17 +0000

    add (non-functional) accounte form tests

---
+++ trunk/source/dunit/uAccountsFormTests.pas
@@ -0,0 +1,60 @@
+﻿unit uAccountsFormTests;
+
+interface
+
+uses
+  TestFrameWork, uRCUtils;
+
+type
+ TTest_uAccountsForm = class(TTestCase)
+ published
+   procedure Test_AddRemoveAccounts;
+ end;
+
+
+
+implementation
+uses SysUtils, Dialogs, System.UITypes;
+
+
+procedure TTest_uAccountsForm.Test_AddRemoveAccounts;
+var
+  FNameOfBook : String;
+begin
+
+{
+Manual Test Cases:
+
+1. Start with no accounts
+2. Add an account, edit, save
+3. Delete the one account, back to no accounts
+  3a. select no on the delete new account confirmation -> one account remains
+  3b. repeat but select yes -> now no accounts, all fields disabled, applicable buttons disabled
+4. Add several accounts, fill in random details on some.
+  -> prompted to save changes when account any time add account pressed
+  -> not prompted if account saved first
+5. Click on a new account tab while editing. -> prompted to save
+  5b. repeat with no unsaved edits, no prompt
+6. click on a different screen tab (like mail) while editing -> prompted to save
+  6b. repeat with no unsaved edits, no prompt.
+7. Add new account, delete without saving, test each dialog option
+8. Delete account from middle
+9. Delete first account.
+10. Reorder accounts
+  -> verify account order "sticks"
+  -> verify account order persists between shutting down the app
+  -> verify rules are modified to continue to point to the correct higher numbered accounts
+11. Delete account with rules
+  -> rules changed to inactive, set to "all accounts"
+  -> rules for higher number accounts shifted
+
+
+
+}
+
+end;
+
+
+initialization
+   TestFramework.RegisterTest(TTest_uAccountsForm.Suite);
+end.
